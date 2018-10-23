b6df217659922582d2f414ac7f0f4cee911f4e0e
Hank Tom <hanktom@gmail.com>
2018-10-23 16:25:03 +0800

    Initial commit

---
+++ app/src/main/java/com/tom/atm/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.tom.atm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view) {
+        String userid = ((EditText)findViewById(R.id.ed_userid)).getText().toString();
+        String passwd = ((EditText)findViewById(R.id.ed_password)).getText().toString();
+        if ("jack".equals(userid) && "1234".equals(passwd)) {
+            finish();
+        }
+    }
+}
