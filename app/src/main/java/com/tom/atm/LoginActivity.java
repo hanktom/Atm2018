0975b5e7a733ed94c39863bd7dd7dc8a4e028328
Hank Tom <hanktom@gmail.com>
2018-11-13 15:23:38 +0800

    remember userid

--- app/src/main/java/com/tom/atm/LoginActivity.java
+++ app/src/main/java/com/tom/atm/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.tom.atm;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,12 +12,20 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        EditText edUserid = findViewById(R.id.ed_userid);
+        String userid = getSharedPreferences("atm", MODE_PRIVATE)
+                .getString("USERID", "");
+        edUserid.setText(userid);
     }
 
     public void login(View view) {
         String userid = ((EditText)findViewById(R.id.ed_userid)).getText().toString();
         String passwd = ((EditText)findViewById(R.id.ed_password)).getText().toString();
         if ("jack".equals(userid) && "1234".equals(passwd)) {
+            getSharedPreferences("atm", MODE_PRIVATE)
+                    .edit()
+                    .putString("USERID", userid)
+                    .apply();
             finish();
         }
     }
