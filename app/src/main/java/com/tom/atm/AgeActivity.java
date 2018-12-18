0ae26e047b49b99b13294c8dd10efaefdd01afa1
Hank Tom <hanktom@gmail.com>
2018-12-18 15:59:55 +0800

    RecyclerView click on item change age

--- app/src/main/java/com/tom/atm/AgeActivity.java
+++ app/src/main/java/com/tom/atm/AgeActivity.java
@@ -7,23 +7,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class AgeActivity extends BaseActivity {
-    int[] numbers = {19,20,21,22,23,24,25};
+    int[] numbers = {16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32};
+    String[] rainbow = null;
+    private EditText edAge;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_age);
+        rainbow = getResources().getStringArray(R.array.rainbow);
         RecyclerView recyclerView = findViewById(R.id.recycler);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         //TODO: set adapter
         AgeAdapter adapter = new AgeAdapter();
         recyclerView.setAdapter(adapter);
+        edAge = findViewById(R.id.ed_age);
     }
 
     public void next(View view) {
@@ -46,11 +52,17 @@ public class AgeActivity extends BaseActivity {
         }
 
         @Override
-        public void onBindViewHolder(@NonNull AgeHolder holder, int position) {
+        public void onBindViewHolder(@NonNull AgeHolder holder, final int position) {
             holder.ageText.setText(numbers[position]+"");
-            if (numbers[position] == 19) {
-                holder.ageText.setTextColor(Color.RED);
-            }
+            holder.itemView.setBackgroundColor(Color.parseColor(rainbow[position%7]));
+//            Log.d("AgeActivity", "onBindViewHolder: " + Integer.parseInt(rainbow[position%7]));
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.d("AgeActivity", "onClick: " + numbers[position]);
+                    edAge.setText(numbers[position]+"");
+                }
+            });
         }
 
         @Override
